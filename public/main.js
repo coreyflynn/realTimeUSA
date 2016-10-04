703cbd298175db0f4bb157fd2765c1be45877a97
Corey Flynn <flynn.corey@gmail.com>
2016-10-04 15:55:45 -0400

    Adds sounds

--- public/main.js
+++ public/main.js
@@ -1,6 +1,17 @@
 // Set up the map projection that we would like to use
 var projection = d3.geo.albersUsa();
 
+// set up a sound to play when a new tweet comes in
+var sounds = [];
+for (var i = 1; i < 27; i++) {
+  var str = i.toString();
+  sounds.push(new Howl({
+    src: ['sounds/c' + ('000'+str).substring(str.length) + '.mp3'],
+    volume: 0.25,
+  }))
+}
+
+
 if(io !== undefined) {
   socket = io.connect('http://localhost:3000');
 
@@ -20,6 +31,9 @@ if(io !== undefined) {
     if (tweet.coordinates) {
       projectedCoords = projection(tweet.coordinates.coordinates);
       if (projectedCoords) {
+        console.log(projectedCoords);
+        var sound = sounds[Math.floor(Math.random() * sounds.length)];
+        sound.play();
         var id = new Date().getTime();
         svg.append("circle")
           .attr('id', 'circle' + id)
@@ -29,10 +43,10 @@ if(io !== undefined) {
           .attr("transform", function() {
             return "translate(" + projection(tweet.coordinates.coordinates) + ")";
           });
-        d3.selectAll('#circle' + id).transition().duration(1000)
+        d3.selectAll('#circle' + id).transition().duration(0)
           .attr('r',20)
           .attr('opacity', 0.5)
-          .transition().duration(1000).delay(2000)
+          .transition().duration(1000).delay(1000)
           .attr('r',5)
           .attr('opacity', 0)
           .remove();
